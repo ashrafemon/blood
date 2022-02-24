72c6846b8eb5eee8acb53dda03ac92df41a19b81
Ashraf Emon <ashraf.emon143@gmail.com>
2022-02-24 14:35:18 +0600

    App View Change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,13 +13,13 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Noto+Serif+Gujarati:wght@600&family=Open+Sans:wght@400;600;700&family=Permanent+Marker&family=Philosopher:wght@400;700&family=Play:wght@700&family=Poppins:wght@100;400;500;600;700&family=Quicksand:wght@400;500&display=swap"
         rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div id="root"></div>
 
-    <div>{{}}</div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
